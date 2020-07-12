b1751ad1644d5c96f155f6e7c9453d13118192cf
Mayara Cardoso <mayarakardoso@gmail.com>
2020-07-12 18:11:31 -0300

    feat: criada animação com angular animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('myAnimation', [
+      state('small', style({
+        transform: 'scale(1)',
+      })),
+      state('large', style({
+        transform: 'scale(1.2)',
+      })),
+      transition('small <=> large', animate('500ms ease-in'))
+    ])
+  ]
 })
 export class AppComponent {
   title = 'my-animations';
+  state ='small';
+
+  animateMe() {
+    this.state = (this.state === 'small' ? 'large' : 'small');
+  }
 }
